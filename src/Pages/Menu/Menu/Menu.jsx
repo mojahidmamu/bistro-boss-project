35a56acc232ac24d739678e5136794030ad64c2f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-02 07:13:28 +0600

    added new something

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -4,11 +4,10 @@ import Cover from "../../Shared/Cover/Cover";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import menuImg from '../../../assets/menu/banner3.jpg'
 import PopularMenu from "../../Homes/PopularMenu/PopularMenu";
-import Slide1 from '../../../assets/home/slide1.jpg';
-import Slide2 from '../../../assets/home/slide2.jpg';
-import Slide3 from '../../../assets/home/slide3.jpg';
-import Slide4 from '../../../assets/home/slide4.jpg';
-import Slide5 from '../../../assets/home/slide5.jpg';
+import Slide1 from '../../../assets/menu/dessert-bg.jpeg';
+import Slide2 from '../../../assets/menu/pizza-bg.jpg';
+import Slide3 from '../../../assets/menu/salad-bg.jpg';
+import Slide4 from '../../../assets/menu/soup-bg.jpg'; 
 
 const Menu = () => {
   return (
@@ -18,11 +17,11 @@ const Menu = () => {
       </Helmet>
       <Cover img={menuImg} title='OUR MENU' subTitle='Would You Like To Try A Dish ?'></Cover>
        <PopularMenu></PopularMenu>
-      <Cover img={Slide1} title='DESSERTS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
+      <Cover img={Slide1} title='PIZZA' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
        <PopularMenu></PopularMenu>
       <Cover img={Slide2} title='SLALDS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
        <PopularMenu></PopularMenu>
-      <Cover img={Slide3} title='PIZZA' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
+      <Cover img={Slide3} title='DESSERTS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
        <PopularMenu></PopularMenu>
       <Cover img={Slide4} title='SOUPS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></Cover>
        <PopularMenu></PopularMenu>
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,7 +1,6 @@
- 
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
-import ShopImage from '../../../assets/shop/banner2.jpg'
+import ShopImage from "../../../assets/shop/banner2.jpg";
 
 const Order = () => {
   return (
@@ -9,8 +8,22 @@ const Order = () => {
       <Helmet>
         <title>Bistro | Order Food</title>
       </Helmet>
-       
-      <Cover img={ShopImage} title='OUR SHOP' subTitle='Would You Like To Try To Dish ?'></Cover>
+
+      <Cover
+        img={ShopImage}
+        title="OUR SHOP"
+        subTitle="Would You Like To Try To Dish ?"
+      ></Cover>
+      <div>
+        <div role="tablist" className="tabs tabs-lifted">
+           
+          <a role="tab" className="tab">DESSERTS</a>
+          <a role="tab" className="tab">DRINTKS</a>
+          <a role="tab" className="tab">SALAD</a>
+          <a role="tab" className="tab">PIZZA</a>
+          <a role="tab" className="tab">DRINTKS</a>
+        </div>
+      </div>
     </div>
   );
 };
