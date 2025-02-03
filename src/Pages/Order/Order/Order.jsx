58fadb492ddc1be39bb66a7ae44c5c3ec2101d71
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-03 09:29:12 +0600

    tab

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -2,8 +2,12 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import ShopImage from "../../../assets/shop/banner2.jpg";
 import { Link } from "react-router-dom";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css'
+import { useState } from "react";
 
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <div>
       <Helmet>
@@ -15,17 +19,27 @@ const Order = () => {
         title="OUR SHOP"
         subTitle="Would You Like To Try To Dish ?"
       ></Cover>
-      <div>
-        <div role="tablist" className="tabs tabs-lifted">
-           
-          {/* <Link to='DESSERTS'><a role="tab" className="tab">DESSERTS</a></Link> */}
+      <div role="tablist" className="tabs tabs-lifted justify-center my-5 ml-3">
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>DESSERTS</Tab>
+          <Tab>SALAD</Tab>
+          <Tab>PIZZA</Tab>
+          <Tab>DRINKS</Tab>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+        </TabList>
+      </Tabs>
+
+      </div>
+      
+        <div role="tablist" className="tabs tabs-lifted"> 
           <a role="tab" className="tab"> <Link to='/DESSERTS'>DESSERTS</Link> </a>
           <a role="tab" className="tab"> <Link to='/SALAD'>SALAD</Link> </a>
-          <a role="tab" className="tab"> <Link to='/PIZZA'>PIZZA</Link></a>
+          <a role="tab" className="tab tab-active"> <Link to='/PIZZA'>PIZZA</Link></a>
           <a role="tab" className="tab"> <Link to='/DRINTKS'>DRINTKS</Link> </a>
-          <a role="tab" className="tab">Mojahid </a>
+           
         </div>
-      </div>
     </div>
   );
 };
